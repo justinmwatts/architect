6e30a25a9d5093aa81b6335df32ca4195b405600
Justin Watts <jwatts@redventures.com>
2016-09-27 22:55:58 -0400

    removes nav-toggle update

--- src/js/app.js
+++ src/js/app.js
@@ -3,22 +3,7 @@
 (function ($) {
 	$(document).on('ready', function () {
 
-		// Nav Toggle
-		function navToggle(e) {
-			e.preventDefault();
 
-			var activeClass = 'is-active';
-			var navbarToggleClass = '.js-navbar-toggle';
-
-			if ($(navbarToggleClass).hasClass(activeClass) === true) {
-				$(navbarToggleClass).removeClass(activeClass);
-			}
-			else {
-				$(navbarToggleClass).addClass(activeClass);
-			}
-		}
-
-		$('.js-navbar-toggle').on('click', navToggle);
 
 	});
 })(jQuery);
